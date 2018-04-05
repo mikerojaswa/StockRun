fff0e6fe32105e122ad43e38d4a8876a68be042e
Michael Rojas <mikerojaswa@gmail.com>
2018-04-04 22:58:44 -0700

    init

---
+++ StockRun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockRun
+//
+//  Created by Michael Rojas on 4/4/18.
+//  Copyright © 2018 Michael Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
